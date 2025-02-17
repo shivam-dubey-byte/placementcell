fd1e75d94dd2e21a4fe534c4d61b6c76a09c9220
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2025-02-18 01:42:19 +0530

    0.2.14

--- request-backend/src/app.js
+++ request-backend/src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDB = require("./connectDB");
 
-//const resumeRoutes = require("./routes/routes");
+const resumeRoutes = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   //res.json(items);
   res.send('Request Backend Working');
 });
-//app.use("/api", routes);
+app.use("/api", routes);
 
 //connectDB();
 //https://docs.google.com/forms/d/e/1FAIpQLSdfkgQU261P8_KezoMyQw_xBctCQfr5sp4S-TmPB26eBK99CQ/
